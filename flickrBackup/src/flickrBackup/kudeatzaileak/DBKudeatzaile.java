108c2e2f1c5631aa7a4a72e52fa353e6b364cce6
Zihara <araizaloidab@gmail.com>
2016-12-17 15:57:37 +0100

    DB-a

--- flickrBackup/src/flickrBackup/kudeatzaileak/DBKudeatzaile.java
+++ flickrBackup/src/flickrBackup/kudeatzaileak/DBKudeatzaile.java
@@ -1,6 +1,13 @@
 package flickrBackup.kudeatzaileak;
 
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+
 
 public class DBKudeatzaile {
 
@@ -8,15 +15,35 @@ public class DBKudeatzaile {
 
 	private void conOpen() {
 		try {
-			String userName = "flickrbackup";
-			String password = "1234ABCD";
-			String url = "jdbc:mysql://localhost:3306/Flickrbackup";
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			conn = (Connection) DriverManager.getConnection(url, userName, password);
+//			String userName = "teletxe";
+//			String password = "softing2010";
+			String url = "jdbc:sqlite:isadflickr.db";
+		//	Class.forName("com.mysql.jdbc.Driver").newInstance();
+			Class.forName("org.sqlite.JDBC").newInstance();
+			
+			conn = (Connection) DriverManager.getConnection(url);
+			//, userName,	password);
 			System.out.println("Database connection established");
 		} catch (Exception e) {
 			System.err.println("Cannot connect to database server");
 		}
+	}
+
+
+
+	private void conClose() {
+
+		if (conn != null)
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		
+		System.out.println("Database connection terminated");
+
 	}
 
 	private ResultSet query(Statement s, String query) {
@@ -24,8 +51,8 @@ public class DBKudeatzaile {
 		ResultSet rs = null;
 
 		try {
-			s.executeQuery(query);
-			rs = s.getResultSet();
+			rs = s.executeQuery(query);
+			// rs = s.getResultSet();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -38,28 +65,42 @@ public class DBKudeatzaile {
 
 	private DBKudeatzaile() {
 		this.conOpen();
+
 	}
 
 	public static DBKudeatzaile getInstantzia() {
 		return instantzia;
 	}
 
+	//
 	public ResultSet execSQL(String query) {
 		int count = 0;
 		Statement s = null;
 		ResultSet rs = null;
+		
 		try {
 			s = (Statement) conn.createStatement();
 			if (query.toLowerCase().indexOf("select") == 0) {
 				// select agindu bat
 				rs = this.query(s, query);
+				
 			} else {
 				// update, delete, create agindu bat
 				count = s.executeUpdate(query);
+				System.out.println(count + " rows affected");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+//		finally {
+//			if (s != null)
+//				try {
+//					s.close();
+//				} catch (SQLException e) {
+//					e.printStackTrace();
+//				}
+//		}
+
 		return rs;
 	}
 }
